6067c368bd64331002e92c3cd2cb5ffc5e51b697
keval718 <kevalvirat@gmail.com>
2017-12-30 22:50:10 +0530

    upadte by user id services done

--- models/userm.js
+++ models/userm.js
@@ -5,7 +5,10 @@ var user={
     {
         return db.query("select * from user_tbl",callback);
     },
-
+    getUserById:function(id,callback)
+    {
+        return db.query("select * from user_tbl where user_email_id=?",[id],callback);
+    },
     addUser:function(User,callback)
     {
      //   return db.query("insert into car_tbl values(car_name=?,?,?,?,?,?,?,?)",[Car.car_name,Car.car_color,Car.car_type,Car.car_img,Car.car_rate,Car.car_details,Car.car_category,Car.fk_traveller_id],callback);
@@ -20,7 +23,7 @@ var user={
 
     updateUser:function(id,User,callback)
     {
-        return db.query("update user_tbl set user_password=?,user_name=?,user_address=?,user_DO_B=?,user_gender=?,user_photo=?,user_mobile_no=? where user_email_id=?",[User.user_password,User.user_name,User.user_address,User.user_DO_B,User.user_gender,User.user_photo,User.user_mobile_no,id],callback);
+        return db.query("update user_tbl set user_name=?,user_address=?,user_DO_B=?,user_gender=?,user_photo=?,user_mobile_no=? where user_email_id=?",[User.user_name,User.user_address,User.user_DO_B,User.user_gender,User.user_photo,User.user_mobile_no,id],callback);
     }
 };
 module.exports=user;
--- routes/user.js
+++ routes/user.js
@@ -15,6 +15,18 @@ router.get('/',function(req,res,next){
     });
 });
 
+router.get('/:id',function(req,res,next){
+    User.getUserById(req.params.id,function(err,rows){
+        if(err)
+        {
+            res.json(err);
+        }
+        else
+        {
+            res.json(rows);
+        }
+    });
+});
 router.post('/',function(req,res,next){
     User.addUser(req.body,function(err,count){
         if(err)
