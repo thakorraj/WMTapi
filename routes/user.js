119b18b8d75c2ee4e8b83e2a572062c5285f47c5
P@rmar117 <jollyprashil@gmail.com>
2018-01-01 15:11:50 +0530

    traveller service done

--- routes/user.js
+++ routes/user.js
@@ -1,69 +1,63 @@
 var express = require('express');
 var router = express.Router();
-var User=require('../models/userm');
+var User = require('../models/userm');
 
-router.get('/',function(req,res,next){
-    User.getAllUser(function(err,rows){
-        if(err)
-        {
+router.get('/', function(req, res, next) {
+    User.getAllUser(function(err, rows) {
+        if (err) {
             res.json(err);
-        }
-        else
-        {
+        } else {
             res.json(rows);
         }
     });
 });
 
-router.get('/:id',function(req,res,next){
-    User.getUserById(req.params.id,function(err,rows){
-        if(err)
-        {
+router.get('/:id', function(req, res, next) {
+    User.getUserById(req.params.id, function(err, rows) {
+        if (err) {
             res.json(err);
-        }
-        else
-        {
+        } else {
             res.json(rows);
         }
     });
 });
-router.post('/',function(req,res,next){
-    User.addUser(req.body,function(err,count){
-        if(err)
-        {
+router.post('/', function(req, res, next) {
+    User.addUser(req.body, function(err, count) {
+        if (err) {
             res.json(err);
-        }
-        else
-        {
+        } else {
             res.json(req.body);
         }
     });
 });
 
-router.delete('/:id',function(req,res,next){
-    User.deleteUser(req.params.id,function(err,count){
-        if(err)
-        {
+router.delete('/:id', function(req, res, next) {
+    User.deleteUser(req.params.id, function(err, count) {
+        if (err) {
             res.json(err);
-        }
-        else
-        {
+        } else {
             res.json(count);
         }
     });
 });
 
-router.put('/:id',function(req,res,next){
-    User.updateUser(req.params.id,req.body,function(err,rows){
-        if(err)
-        {
+router.put('/:id', function(req, res, next) {
+    User.updateUser(req.params.id, req.body, function(err, rows) {
+        if (err) {
             res.json(err);
-        }
-        else
-        {
+        } else {
             res.json(rows);
         }
     });
+    router.get('/', function(req, res, next) {
+        User.user_count(function(err, rows) {
+            if (err) {
+                res.json(err);
+            } else {
+                res.json(rows);
+            }
+        });
+    });
 });
 
-module.exports=router;
+module.exports = router;
