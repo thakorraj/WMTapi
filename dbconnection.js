aa2ccd4b08ebc8e829092cde3ff562e5a1a02a64
keval718 <kevalvirat@gmail.com>
2018-04-26 15:31:01 +0530

    All Done

--- dbconnection.js
+++ dbconnection.js
@@ -5,4 +5,11 @@ var connection=mysql.createPool({
     password:'',
     database:'WMT'
 });
+
+// var connection=mysql.createPool({
+//     host:'wishmytrip.db.9462939.e8f.hostedresource.net',
+//     user:'wishmytrip',
+//     password:'Raj@1212',
+//     database:'wishmytrip'
+// });
 module.exports=connection;
