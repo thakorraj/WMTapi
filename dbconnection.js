c9dcfcbcdff09131e36c94a6bfe518b97f5a5a4a
thakorraj <thakorraj80@yahoo.com>
2018-04-27 10:21:36 +0530

    hello

--- dbconnection.js
+++ dbconnection.js
@@ -1,15 +1,15 @@
-var mysql=require('mysql');
-var connection=mysql.createPool({
+var mysql = require('mysql');
+/* var connection=mysql.createPool({
     host:'localhost',
     user:'root',
     password:'',
     database:'WMT'
-});
+}); */
 
-// var connection=mysql.createPool({
-//     host:'wishmytrip.db.9462939.e8f.hostedresource.net',
-//     user:'wishmytrip',
-//     password:'Raj@1212',
-//     database:'wishmytrip'
-// });
-module.exports=connection;
+var connection = mysql.createPool({
+    host: 'wishmytrip.db.9462939.e8f.hostedresource.net',
+    user: 'wishmytrip',
+    password: 'Raj@1212',
+    database: 'wishmytrip'
+});
+module.exports = connection;
